cc4dfff7f5b51096b6b70596e3a5ae79b3b7efb2
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-03 17:53:58 -0700

    importing all components and adding routes

--- int-aid-hackathon/src/App.js
+++ int-aid-hackathon/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import ScrollTop from './Components/ScrollTop/ScrollTop';
+import Nav from './Components/Nav/Nav';
+import LandingPage from './Components/LandingPage/LandingPage';
+import CreateDonor from './Components/CreateDonor/CreateDonor';
+import CreateTeacher from './Components/CreateTeacher/CreateTeacher';
+import TeacherProfile from './Components/TeacherProfile/TeacherProfile';
+import SearchPage from './Components/SearchPage/SearchPage';
+import About from './Components/About/About';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <main>
+        <ScrollTop>
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/create-donor" element={<CreateDonor/>} />
+            <Route path="/create-teacher" element={<CreateTeacher/>} />
+            <Route path="/teacher-profile" element={<TeacherProfile/>} />
+            <Route path="/search" element={<SearchPage/>} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </ScrollTop>
+      </main>
+      <Footer/>
     </div>
   );
 }
