5a8474805b0e0db23a9912ad59410bfb5be5be44
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 16:49:27 -0700

    importing new components and adding routes

--- int-aid-hackathon/src/App.js
+++ int-aid-hackathon/src/App.js
@@ -3,9 +3,13 @@ import { Route, Routes } from "react-router-dom";
 
 import ScrollTop from './Components/ScrollTop/ScrollTop';
 import Nav from './Components/Nav/Nav';
+import SignUp from './Components/SignUp';
+import LoginModal from './Components/LoginModal'
 import LandingPage from './Components/LandingPage';
 import CreateDonor from './Components/CreateDonor';
-import CreateTeacher from './Components/CreateTeacher';
+import CreateTeacher from './Components/TeacherSignUp/CreateTeacher';
+import DetailPage from './Components/TeacherSignUp/Details';
+import NamePage from './Components/TeacherSignUp/Name';
 import TeacherProfile from './Components/TeacherProfile';
 import SearchPage from './Components/SearchPage';
 import About from './Components/About';
@@ -19,8 +23,12 @@ function App() {
         <ScrollTop>
           <Routes>
             <Route path="/" element={<LandingPage/>} />
+            <Route path="/sign-up" element={<SignUp/>} />
+            <Route path="/login" element={<LoginModal/>} />
             <Route path="/create-donor" element={<CreateDonor/>} />
             <Route path="/create-teacher" element={<CreateTeacher/>} />
+            <Route path="/details" element={<DetailPage/>} />
+            <Route path="/name" element={<NamePage/>} />
             <Route path="/teacher-profile" element={<TeacherProfile/>} />
             <Route path="/search" element={<SearchPage/>} />
             <Route path="/about" element={<About/>} />
