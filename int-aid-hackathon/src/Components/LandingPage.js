fd09cb1abb36f3544a110045a0eccdcb1f8385a6
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 16:50:17 -0700

    adding UI components for Landing Page

--- int-aid-hackathon/src/Components/LandingPage.js
+++ int-aid-hackathon/src/Components/LandingPage.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 const LandingPage = (props) => {
     return (
         <div>
-            <h1>Landing Page</h1>
-            <div className="create-user-buttons">
-                <Link className="link" to="/create-teacher">
-                    <button className="btn  btn-primary">Create Teacher</button>
-                </Link>
-                <Link className="link" to="/create-donor">
-                    <button className="btn btn-primary">Create Donor</button>
-                </Link> 
+            <div className="hero min-h-full bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-xlg">
+                    <h1 className="text-4xl font-bold">WE HELP EDUCATORS AROUND THE <br/> GLOBE GET THE SUPPLIES THEY NEED</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="hero min-h-xlg bg-white-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-m">
+                    <p className="text-2xl py-7">Need supplies? Want to give supplies? <br/> You're in the right place.</p>
+                    <Link className="link" to="/sign-up">
+                        <button className="btn  btn-primary">Let's Get Started!</button>
+                    </Link>
+                    <Link to="/login"><h6>Already Have An Account? Sign In.</h6></Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
