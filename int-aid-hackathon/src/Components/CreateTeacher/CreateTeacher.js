202a7d42591850883d163e3a7064776f9bcadf52
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-04 01:52:10 -0600

    todos

--- int-aid-hackathon/src/Components/CreateTeacher/CreateTeacher.js
+++ int-aid-hackathon/src/Components/CreateTeacher/CreateTeacher.js
@@ -3,24 +3,23 @@ import { dbClient } from "../../services/dbClient"
 
 
 const CreateTeacher = (props) => {
+    //TODO: determine if state or refs is ideal. Component should unmount on nav, removing ref from the dom, so it *shouldn't* be exposed. 
 const emailRef = useRef()
 const passRef = useRef()
 
+//handle click
 const handleClick = async (event) => {
     event.preventDefault()
-    console.log(dbClient)
-console.log(emailRef.current.value)
-console.log(passRef.current.value)
+ 
 
 
+// destructure return from auth signup method
     const { user, session, error } = await dbClient.auth.signUp({
         email: emailRef.current.value ,
         password: passRef.current.value
       }) 
-
+//TODO: error handling, pending ui?, navigation
       if (user) {
-          console.log(session)
-          console.log("woo")
           return console.log(user)
       } else { 
           return console.error(error, 401)
@@ -32,7 +31,7 @@ console.log(passRef.current.value)
         <div>
             <section>
                 <p>
-               
+               {/* TODO: validate input, feedback (snackbar or alert) */}
                        <input ref={emailRef} type="text" placeholder="email"/>
 
                        <input ref={passRef} type="text" placeholder="pass"/>
--- int-aid-hackathon/src/services/dbClient.js
+++ int-aid-hackathon/src/services/dbClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+//get non-secret env vars. TODO: ENABLE ROW LEVEL SECURITY FOR DB, URL AND PUBLIC KEY WILL BE EXPOSED ON CLIENT!!
+
 let supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 let supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
+//instantiate instance of SupabaseClient class from supabase-js
 export const dbClient = createClient(supabaseUrl, supabaseAnonKey)
 
