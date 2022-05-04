c8d3b6e5923f0511fae1f808a5a3b930e7a929a1
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-04 02:53:13 -0600

    login utility function

--- int-aid-hackathon/src/Components/CreateTeacher/CreateTeacher.js
+++ int-aid-hackathon/src/Components/CreateTeacher/CreateTeacher.js
@@ -1,48 +1,48 @@
-import {useRef} from "react"
-import { dbClient } from "../../services/dbClient"
-
+import { useRef } from "react";
+import { dbClient } from "../../services/dbClient";
+import { logIn } from "../../utilities/logIn";
 
 const CreateTeacher = (props) => {
-    //TODO: determine if state or refs is ideal. Component should unmount on nav, removing ref from the dom, so it *shouldn't* be exposed. 
-const emailRef = useRef()
-const passRef = useRef()
 
-//handle click
-const handleClick = async (event) => {
-    event.preventDefault()
- 
+  //TODO: determine if state or refs is ideal. Component should unmount on nav, removing ref from the dom, so it *shouldn't* be exposed.
+  const emailRef = useRef();
+  const passRef = useRef();
 
-
-// destructure return from auth signup method
+  //handle click
+  const handleClick = async (event) => {
+    event.preventDefault();
+    let email = emailRef.current.value
+    let password = passRef.current.value
+    // destructure return from auth signup method
     const { user, session, error } = await dbClient.auth.signUp({
-        email: emailRef.current.value ,
-        password: passRef.current.value
-      }) 
-//TODO: error handling, pending ui?, navigation
-      if (user) {
-          return console.log(user)
-      } else { 
-          return console.error(error, 401)
-      }
+      email: email,
+      password: password 
+    });
 
+    //TODO: error handling, pending ui?, navigation
+    if (user) {
+        let currentUser = await logIn(email, password)
+        console.log(currentUser)
+      return console.log(user);
+    } else {
+      return console.error(error, 401);
+    }
+  };
 
-}
-    return (
-        <div>
-            <section>
-                <p>
-               {/* TODO: validate input, feedback (snackbar or alert) */}
-                       <input ref={emailRef} type="text" placeholder="email"/>
+  return (
+    <div>
+      <section>
+        <p>
+          {/* TODO: validate input, feedback (snackbar or alert) */}
+          <input ref={emailRef} type="text" placeholder="email" />
 
-                       <input ref={passRef} type="text" placeholder="pass"/>
+          <input ref={passRef} type="text" placeholder="pass" />
 
-                       <button onClick={(e)=> handleClick(e)}>Submit</button>
-
-
-                </p>
-            </section>
-        </div>
-    )
-}
+          <button onClick={(e) => handleClick(e)}>Submit</button>
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default CreateTeacher;
---
+++ int-aid-hackathon/src/utilities/logIn.js
@@ -0,0 +1,16 @@
+import { dbClient } from "../services/dbClient"
+
+export const logIn = async (email, pass) => {
+    
+   let {session, user, error} = await dbClient.auth.signIn(email, pass)
+
+   if (user) { 
+       let res = {
+           user: user,
+           session: session
+       }
+       return res
+   } else {
+       return error
+   }
+}
