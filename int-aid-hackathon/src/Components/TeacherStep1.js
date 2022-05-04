c62878a51382e27bdf3c2fc0b01352942cc50fc3
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 14:45:46 -0700

    adding component for first step of teacher sign up process

---
+++ int-aid-hackathon/src/Components/TeacherStep1.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const TeacherStep1 = (props) => {
+    return (
+        <div>
+            <h1>Steps to creation</h1>
+        </div>
+    )
+}
+
+export default TeacherStep1;
