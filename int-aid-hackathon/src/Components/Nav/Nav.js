e1809b6a17948977f84d02e7bf9f08b7f94a1f87
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-03 17:52:25 -0700

    adding links to components

--- int-aid-hackathon/src/Components/Nav/Nav.js
+++ int-aid-hackathon/src/Components/Nav/Nav.js
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Nav = (props) => {
     return (
-        <div>
-            <h1>Schoolhouse Seven</h1>
-        </div>
+        <nav>
+            <div className="links">
+                <Link className="link" to="/">
+                    <h3>Home</h3>
+                </Link>
+                <Link className="link" to="/about">
+                    <h3>About</h3>
+                </Link>
+                <Link className="link" to="/teacher-profile">
+                    <h3>Profile</h3>
+                </Link>
+                <Link className="link" to="/search">
+                    <h3>Search Listings</h3>
+                </Link>
+            </div>
+        </nav>
     )
 }
 
