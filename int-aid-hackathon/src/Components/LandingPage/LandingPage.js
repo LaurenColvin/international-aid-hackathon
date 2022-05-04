3a5e440b3e907c886692c022da95bde7539d91b8
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-03 17:53:06 -0700

    adding buttons for create users components

--- int-aid-hackathon/src/Components/LandingPage/LandingPage.js
+++ int-aid-hackathon/src/Components/LandingPage/LandingPage.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const LandingPage = (props) => {
     return (
         <div>
             <h1>Landing Page</h1>
+            <div className="create-user-buttons">
+                <Link className="link" to="/create-teacher">
+                    <button>Create Teacher</button>
+                </Link>
+                <Link className="link" to="/create-donor">
+                    <button>Create Donor</button>
+                </Link> 
+            </div>
         </div>
     )
 }
