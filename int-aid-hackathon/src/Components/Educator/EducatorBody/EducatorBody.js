66361260931f613c23f955f1d710843f993f5788
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-04 20:03:57 -0600

    mock wishlist array

--- int-aid-hackathon/src/Components/Educator/EducatorBody/EducatorBody.js
+++ int-aid-hackathon/src/Components/Educator/EducatorBody/EducatorBody.js
@@ -1,5 +1,61 @@
 import React from 'react'
+const itemArray = [
+    {
+        name: "pencils",
+        numberNeeded: 7,
+        imgUrl: "https://www.businesssourceproducts.com/images/650/DIX13882.jpg"
+    },
+    {
+        name: "pens",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
 
+    },
+    {
+        name: "notebooks",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "thingsdso",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "binders",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "stuff",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "ee",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "waaa",
+        numberNeeded: 7,
+        imgUrl: "https://cdn.thewirecutter.com/wp-content/media/2021/09/pens-2048px-6470.jpg",
+
+    },
+    {
+        name: "werrrro",
+        numberNeeded: 7,
+        imgUrl: "https://m.media-amazon.com/images/I/31VXrrSQpIL._AC_.jpg",
+
+    },
+    
+
+]
 function EducatorBody() {
   return (
     <div className='flex flex-row justify-around'>
