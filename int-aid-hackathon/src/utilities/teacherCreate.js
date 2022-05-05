a66f6f2698e6d80e9d14b089de59cf9e97522303
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-05 03:18:21 -0600

    page two submit

--- int-aid-hackathon/src/utilities/teacherCreate.js
+++ int-aid-hackathon/src/utilities/teacherCreate.js
@@ -24,3 +24,103 @@ if (user) {
     
 }
 
+
+//didn't add agerange --grace
+export const teacherSubmitTwo = async ({id, schoolName, country, schoolAddress, city, postalCode}) => {
+  
+    const {data: user, error} = await dbClient
+    .from("teacher_profiles")
+    .update({
+        school: `${schoolName}`,
+        country: `${country}`,
+        address: `${schoolAddress}`,
+        city_town: `${city}`,
+        postalcode: `${postalCode}`,
+    })
+    .match({owner: id})
+
+
+    //TODO: ERROR HANDLING
+
+if (error) {
+    console.error(error)
+}
+if (user) {
+    console.log(user)
+    return user
+}
+    
+}
+
+//only made room for 1 image --grace
+export const teacherSubmitThree = async ({id, userImg}) => {
+    console.log(id, userImg)
+    const {data: user, error} = await dbClient
+    .from("teacher_profiles")
+    .update({
+        user_img_url: `${userImg}`,
+    })
+    .match({owner: id})
+
+    //TODO: ERROR HANDLING
+
+if (error) {
+    console.error(error)
+}
+if (user) {
+    console.log(user)
+    return user
+}
+    
+}
+export const teacherSubmitFour = async ({id, q1, q2, q3}) => {
+    console.log(id, q1, q2, q3)
+    const {data: user, error} = await dbClient
+    .from("teacher_profiles")
+    .update({
+        q_1: `${q1}`,
+        q_2: `${q2}`,
+        q_3: `${q3}`,
+    })
+    .match({owner: id})
+
+    //TODO: ERROR HANDLING
+
+if (error) {
+    console.error(error)
+}
+if (user) {
+    console.log(user)
+    return user
+}
+    
+}
+
+//no idea what we are doing here
+// below is handling bulk create --grace
+// const { data, error } = await supabase
+//   .from('cities')
+//   .insert([
+//     { name: 'The Shire', country_id: 554 },
+//     { name: 'Rohan', country_id: 555 },
+//   ])
+export const teacherSubmitFive = async ({id}) => {
+ 
+    const {data: user, error} = await dbClient
+    .from("wishlists")
+    .insert({
+       /*  user_img_url: `${userImg}`, */
+    })
+    .match({owner: id})
+
+    //TODO: ERROR HANDLING
+
+if (error) {
+    console.error(error)
+}
+if (user) {
+    console.log(user)
+    return user
+}
+    
+}
