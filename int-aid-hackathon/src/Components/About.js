8c3a6c6f2dfe1217b0c9ca57a9065c035f00a514
lepard-geko <benjamin.nagle22@gmail.com>
2022-05-05 10:46:32 -0700

    create about page

--- int-aid-hackathon/src/Components/About.js
+++ int-aid-hackathon/src/Components/About.js
@@ -1,8 +1,50 @@
+import './About.css'
+
 const About = (props) => {
     return (
         <div>
-            <h2>About Our Team</h2>
-        </div>
+            <h1>THE STORY</h1>
+            <p>During the International Aid Hackathon, our team was tasked with addressing a problem related to education. With so many project possibilities, we began to do research and brainstroming to narrow our focus: </p>
+            <p id='imf'>“In developing countries, school attendance, especially for girls, is far from universal, and many children drop out of school before completing their primary education. Many children who do attend school receive an inadequate education because of poorly trained, underpaid teachers, overcrowded classrooms, and a lack of basic teaching tools such as textbooks, blackboards, and pens and paper.” - IMF.org</p>
+            <p>Navigating the problem of education in developing countries is a huge task, but we honed in on one specific area - school supplies. </p>
+            <p>We spoke to an educator who spent time teaching in Kenya. She told us of a classroom where every student had to share a single piece of chalk. </p>
+            <p>This is how Chalkbox was born. Chalkbox is a way for teachers to request the supplies they need and for those willing to help to step in and provide basic teaching tools.  </p>
+        <h1>MEET THE TEAM</h1>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02Q73AJ8HZ-a3abf5bb94c9-512" alt=""/>
+            <figcaption>Lauren Colvin</figcaption>
+            <figcaption>Software Developer</figcaption>
+        </figure>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02ET36PZJ5-cc339736dcad-512" alt=""/>
+            <figcaption>Benjamin Nagle</figcaption>
+            <figcaption>Software Developer</figcaption>
+        </figure>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02TQ2C310E-eee1976b7036-512" alt=""/>
+            <figcaption>Margaret Payne</figcaption>
+            <figcaption>User Experience Designer</figcaption>
+        </figure>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02ULEMD35W-526da3d04aa0-512" alt=""/>
+            <figcaption>Jordan Schaeffer</figcaption>
+            <figcaption>User Experience Designer</figcaption>
+        </figure>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02U51PK61G-gd4754415c0a-512" alt=""/>
+            <figcaption>Chanhee Grace Sung</figcaption>
+            <figcaption>Software Developer</figcaption>
+        </figure>
+        <figure>
+            <img src="https://ca.slack-edge.com/T0351JZQ0-U02EJFFMRCM-6acc93326c93-512" alt=""/>
+            <figcaption>Jordan Walsh</figcaption>
+            <figcaption>Software Developer</figcaption>
+        </figure>
+
+        <p></p>
+        <p></p>
+
+    </div>
     )
 }
 
