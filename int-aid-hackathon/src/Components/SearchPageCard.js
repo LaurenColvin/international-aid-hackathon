a45bf22333caefdedf22adce9e38e44dce98aa94
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 23:40:18 -0700

    adjusting color scheme

--- int-aid-hackathon/src/Components/SearchPageCard.js
+++ int-aid-hackathon/src/Components/SearchPageCard.js
@@ -1,6 +1,6 @@
 const SearchPageCard = (props) => {
     return (
-        <div className="flex justify-between content-center m-10 p-4 bg-slate-200">
+        <div className="flex justify-between content-center m-10 p-4 bg-success">
             <div className="flex content-center">
                 <div class="avatar">
                     <div class="w-24 rounded-full">
@@ -8,11 +8,11 @@ const SearchPageCard = (props) => {
                     </div>
                 </div>
                 <div className="ml-3">
-                    <h4 className="text-xl text-left m-3">School Name | Teacher's Name</h4>
-                    <h4 className="text-xl text-left m-3" >Location</h4>
+                    <h4 className="text-xl text-left m-3 text-white">School Name | Teacher's Name</h4>
+                    <h4 className="text-xl text-left m-3 text-white" >Location</h4>
                 </div>
             </div>
-            <h4 className="text-xl m-7">25 items needed</h4>
+            <h4 className="text-xl m-7 text-white">25 items needed</h4>
         </div>
     )
 }
