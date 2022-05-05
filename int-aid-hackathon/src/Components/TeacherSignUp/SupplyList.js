598f6a8279da8fe09f3bb0619485facbbe6f7cb4
kokopuffz <chanheeya@gmail.com>
2022-05-05 00:33:47 -0500

    working supply list with images fixed

--- int-aid-hackathon/src/Components/TeacherSignUp/SupplyList.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/SupplyList.js
@@ -1,43 +1,73 @@
-import { Link } from "react-router-dom";
-
-import CardWrapper from "../layout/CardWrapper";
-import SupplyListCard from "./SupplyListCard";
+import { Link } from "react-router-dom" 
+import CardWrapper from "../layout/CardWrapper"
+import SupplyListCard from "./SupplyListCard"
+import { useState, useEffect } from "react"
+import { dbClient } from "../../services/dbClient"
 
 const SupplyListPage = (props) => {
-  return (
-      <CardWrapper>
-          <div>
-            <h1 className="text-2xl font-bold text-left p-5 ml-50">Creating Your Account</h1>
-            <ul class="steps">
-                <li class="step step-primary">Step 1 <br/> Educator Information</li>
-                <li class="step step-primary">Step 2 <br/> School Information</li>
-                <li class="step step-primary">Step 3 <br/> Upload Pictures</li>
-                <li class="step step-primary">Step 4 <br/> Add Your Story</li>
-                <li class="step step-primary">Step 5 <br/> Create Supply List</li>
-                <li class="step">Step 6 <br/> Review and Complete</li>
-            </ul>
-            <div className="flex">
-                <div>
-                    <p className="text-2xl text-left p-5">Select the supplies you would like added to your list.</p>
-                    <SupplyListCard/>
-                </div>
-                <div class="artboard phone-6 border-black border-2 bg-slate-200 mt-10">
-                    <h2>Your List</h2>
-                </div>
-            </div>
+  const [allSupplies, setAllSupplies] = useState([])
 
-            {/* TODO: bring form to center */}
-            <div className="create-user-buttons">
-                <Link className="link" to="/story">
-                <button className="btn  btn-primary">Back</button>
-                </Link>
-                <Link className="link" to="/review-and-complete">
-                <button className="btn btn-primary">Next</button>
-                </Link> 
-            </div>
+  useEffect(() => {
+    getItems()
+  }, [])
+
+  const getItems = async () => {
+    const { data, error } = await dbClient.from("items").select()
+    setAllSupplies(data)
+  }
+
+  return (
+    <CardWrapper>
+      <div>
+        <h1 className="text-2xl font-bold text-left p-5 ml-50">
+          Creating Your Account
+        </h1>
+        <ul class="steps">
+          <li class="step step-primary">
+            Step 1 <br /> Educator Information
+          </li>
+          <li class="step step-primary">
+            Step 2 <br /> School Information
+          </li>
+          <li class="step step-primary">
+            Step 3 <br /> Upload Pictures
+          </li>
+          <li class="step step-primary">
+            Step 4 <br /> Add Your Story
+          </li>
+          <li class="step step-primary">
+            Step 5 <br /> Create Supply List
+          </li>
+          <li class="step">
+            Step 6 <br /> Review and Complete
+          </li>
+        </ul>
+        <div className="flex">
+          <div>
+            <p className="text-2xl text-left p-5">
+              Select the supplies you would like added to your list.
+            </p>
+            <SupplyListCard 
+              supplies={allSupplies}
+            />
           </div>
-      </CardWrapper>
+          <div class="artboard phone-6 border-black border-2 bg-slate-200 mt-10">
+            <h2>Your List</h2>
+          </div>
+        </div>
+
+        {/* TODO: bring form to center */}
+        <div className="create-user-buttons">
+          <Link className="link" to="/story">
+            <button className="btn  btn-primary">Back</button>
+          </Link>
+          <Link className="link" to="/review-and-complete">
+            <button className="btn btn-primary">Next</button>
+          </Link>
+        </div>
+      </div>
+    </CardWrapper>
   )
 }
 
-export default SupplyListPage;
+export default SupplyListPage
--- int-aid-hackathon/src/Components/TeacherSignUp/SupplyListCard.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/SupplyListCard.js
@@ -1,17 +1,36 @@
-const SupplyListCard = (props) => {
+import SupplyList from "./SupplyList"
+
+const SupplyListCard = (supplies) => {
+  const allSupplies = supplies.supplies.map((supply) => {
+    console.log("SUPPLIES", supply)
+
     return (
-        <div class="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">Shoes!</h2>
-                <p>Quantity?</p>
-                <div class="card-actions justify-end">
-                <button class="btn btn-primary">+</button>
-                <button class="btn btn-primary">-</button>
-                </div>
-            </div>
+      <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <div key={supply.id}>
+          <figure>
+            <img src={supply.img_url} alt={supply.name} class="relative" />
+          </figure>
         </div>
+        <div class="card-body">
+          <h2 class="card-title">{supply.name}</h2>
+          {/* <p>Quantity?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">+</button>
+            <button class="btn btn-primary">-</button>
+          </div> */}
+          <div className="mt-6">
+            <a
+              href={supply.href}
+              className="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
+            >
+              Add to bag<span className="sr-only">, {supply.name}</span>
+            </a>
+          </div>
+        </div>
+      </div>
     )
+  })
+  return <>{allSupplies}</>
 }
 
-export default SupplyListCard;
+export default SupplyListCard
