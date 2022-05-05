0342d1bb61c798fefaf33fde1b88c861596c74ea
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-05 10:09:51 -0700

    rebasing with dev and merging conflicts

--- int-aid-hackathon/src/Components/TeacherSignUp/Details.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/Details.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { dbClient } from "../../services/dbClient";
 
+import CardWrapper from "../layout/CardWrapper";
+
 const DetailPage = (props) => {
   useEffect(() => {
     const data = dbClient.auth.session()
@@ -11,20 +13,32 @@ const DetailPage = (props) => {
   },[])
   
   return (
+    <CardWrapper>
       <div>
-        <p>Enter your information</p>
-        <p>Customize your profile</p>
-        <p>Choose your items</p>
-          <h1>It's that easy!</h1>
-          <div className="create-user-buttons flex justify-between mt-48">
-            <Link className="link" to="/create-teacher">
-                <button className="btn  btn-accent text-white">Back</button>
-            </Link>
-            <Link className="link" to="/sign-up/1">
-                <button className="btn btn-primary text-white">Next</button>
-            </Link> 
+        <div className="flex justify-evenly mt-28">
+          <div>
+            <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+            <p className="text-center text-xl">Enter your information</p>
           </div>
+          <div>
+          <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+            <p className="text-center text-xl">Customize your profile</p>
+          </div>
+          <div>
+          <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+          <p className="text-center text-xl">Choose your items</p>
+          </div>
+        </div>
+        <div className="create-user-buttons flex justify-between mt-48">
+          <Link className="link" to="/create-teacher">
+              <button className="btn btn-wide btn-accent text-white">Back</button>
+          </Link>
+          <Link className="link" to="/sign-up/1">
+              <button className="btn btn-wide btn-primary text-white">Next</button>
+          </Link> 
+        </div>
       </div>
+    </CardWrapper>
   )
 }
 
