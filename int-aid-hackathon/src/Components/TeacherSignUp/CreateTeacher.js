42a33030e5d09089573a04ab8fc82c7a17ebee7f
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 22:14:01 -0700

    adding colors and styling to buttons and steps UI component

--- int-aid-hackathon/src/Components/TeacherSignUp/CreateTeacher.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/CreateTeacher.js
@@ -6,11 +6,17 @@ const CreateTeacher = (props) => {
   return (
   <CardWrapper>
     <div>
-      <h1>Welcome!</h1>
-      <h1>Let's create your account so you can start receiving supplies.</h1>
-      <div className="create-user-buttons">
+      <div className="hero min-h-xlg">
+        <div className="hero-content text-center">
+          <div className="max-w-m">
+            <h1 className="text-5xl font-bold text-primary text-left mt-40">WELCOME!</h1>
+              <p className="text-4xl py-7 text-black mb-11">Let's create your account so you can start receiving supplies.</p>    
+            </div>
+        </div>
+      </div>
+      <div className="create-user-buttons flex justify-between mt-48">
         <Link className="link" to="/">
-            <button className="btn  btn-primary">Back</button>
+            <button className="btn  btn-accent text-white">Back</button>
         </Link>
         <Link className="link" to="/details">
             <button className="btn btn-primary">Next</button>
--- int-aid-hackathon/src/Components/TeacherSignUp/Details.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/Details.js
@@ -7,12 +7,12 @@ const DetailPage = (props) => {
         <p>Customize your profile</p>
         <p>Choose your items</p>
           <h1>It's that easy!</h1>
-          <div className="create-user-buttons">
-              <Link className="link" to="/create-teacher">
-              <button className="btn  btn-primary">Back</button>
+          <div className="create-user-buttons flex justify-between mt-48">
+            <Link className="link" to="/create-teacher">
+                <button className="btn  btn-accent text-white">Back</button>
             </Link>
             <Link className="link" to="/name">
-              <button className="btn btn-primary">Next</button>
+                <button className="btn btn-primary text-white">Next</button>
             </Link> 
           </div>
       </div>
