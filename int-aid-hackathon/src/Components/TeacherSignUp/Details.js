5d9e0c21aaf5f53825ff1f5c29716cf115102ec1
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-05 10:40:43 -0700

    adjusting margin

--- int-aid-hackathon/src/Components/TeacherSignUp/Details.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/Details.js
@@ -15,7 +15,7 @@ const DetailPage = (props) => {
   return (
     <CardWrapper>
       <div>
-        <div className="flex justify-evenly mt-28">
+        <div className="flex justify-evenly mt-20">
           <div>
             <img className="w-xl h-72 m-11" src="https://cdn.discordapp.com/attachments/971825989174825001/971826068786929724/det-1.png" alt="information-icon"></img>
             <p className="text-center text-xl">Enter your information</p>
@@ -29,7 +29,7 @@ const DetailPage = (props) => {
           <p className="text-center text-xl">Choose your items</p>
           </div>
         </div>
-        <div className="create-user-buttons flex justify-between mt-48">
+        <div className="create-user-buttons flex justify-between mt-36">
           <Link className="link" to="/create-teacher">
               <button className="btn btn-wide btn-accent text-white">Back</button>
           </Link>
