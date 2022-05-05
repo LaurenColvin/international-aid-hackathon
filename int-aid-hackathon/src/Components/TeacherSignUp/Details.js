503266f29851ec6c3bc65b9f93fc64b7828e5b7a
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-05-04 23:41:04 -0700

    adding icons and adjusting styling

--- int-aid-hackathon/src/Components/TeacherSignUp/Details.js
+++ int-aid-hackathon/src/Components/TeacherSignUp/Details.js
@@ -1,21 +1,35 @@
 import { Link } from "react-router-dom";
 
+import CardWrapper from "../layout/CardWrapper";
+
 const DetailPage = (props) => {
   return (
+    <CardWrapper>
       <div>
-        <p>Enter your information</p>
-        <p>Customize your profile</p>
-        <p>Choose your items</p>
-          <h1>It's that easy!</h1>
-          <div className="create-user-buttons flex justify-between mt-48">
-            <Link className="link" to="/create-teacher">
-                <button className="btn  btn-accent text-white">Back</button>
-            </Link>
-            <Link className="link" to="/name">
-                <button className="btn btn-primary text-white">Next</button>
-            </Link> 
+        <div className="flex justify-evenly mt-28">
+          <div>
+            <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+            <p className="text-center text-xl">Enter your information</p>
           </div>
+          <div>
+          <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+            <p className="text-center text-xl">Customize your profile</p>
+          </div>
+          <div>
+          <img className="w-xl h-xl m-11" src="https://static.thenounproject.com/png/420338-200.png" alt="information-icon"></img>
+          <p className="text-center text-xl">Choose your items</p>
+          </div>
+        </div>
+        <div className="create-user-buttons flex justify-between mt-48">
+          <Link className="link" to="/create-teacher">
+              <button className="btn btn-wide btn-accent text-white">Back</button>
+          </Link>
+          <Link className="link" to="/name">
+              <button className="btn btn-wide btn-primary text-white">Next</button>
+          </Link> 
+        </div>
       </div>
+    </CardWrapper>
   )
 }
 
