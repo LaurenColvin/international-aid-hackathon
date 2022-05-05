3630ff9c85488a3014439c3c04af5f8c28eb0eb1
jmwalsh91 <jordanmwalsh@gmail.com>
2022-05-04 21:45:33 -0600

    Skeleton, need to modify global css and change navbar

--- int-aid-hackathon/src/Components/Educator/EducatorTop/TeacherInfo.js
+++ int-aid-hackathon/src/Components/Educator/EducatorTop/TeacherInfo.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function TeacherInfo({user}) {
+function TeacherInfo({ user }) {
   return (
     <div class="grid grid-cols-3 divide-x">
-    <div className="text text-lg text-secondary">{user.schoolName}</div>
+      <div className="text text-lg text-secondary">{user.schoolName}</div>
 
-    <div className="text text-lg text-secondary">{user.grade}</div>
-    <div className="text text-lg text-secondary">{user.location}</div>
-  </div>
-  )
+      <div className="text text-lg text-secondary">{user.grade}</div>
+      <div className="text text-lg text-secondary">{user.location}</div>
+    </div>
+  );
 }
 
-export default TeacherInfo
+export default TeacherInfo;
