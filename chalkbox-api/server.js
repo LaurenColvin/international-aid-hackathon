7f555b8a48759b0c1609a93fcdfc59aa7f8a6673
laurensunrise <lauren@sunriseintegration.com>
2022-05-12 18:49:24 -0600

    creating basic express server

---
+++ chalkbox-api/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+// const itemController = require("./controllers/itemController");
+// const userController = require("./controllers/userController");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("Welcome to chalkbox api. Please try /user or /item to see more");
+});
+
+// app.use("/user", userController);
+// app.use("/item", itemController);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
